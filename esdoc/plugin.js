7dfb8683559748c54fa0c985152fdb1a82b0eaec
skratchdot <git@skratchdot.com>
2015-11-20 14:43:36 -0500

    esdoc plugin: linkify extends

--- esdoc/plugin.js
+++ esdoc/plugin.js
@@ -9,5 +9,12 @@ exports.onHandleHTML = function (ev) {
   $('footer').append(
     ' , Copyright &copy; 2015 <a href="http://skratchdot.com/">skratchdot.com</a>'
   );
+  // linkify extends
+  $('div[data-ice="extendsChain"] div').each(function () {
+    const items = $(this).find('span').text().split('~');
+    if (items.length === 2) {
+      $(this).html(`<a href="./class/${items[0]}.js~${items[1]}.html">${items[1]}</a>`);
+    }
+  });
   ev.data.html = $.html();
 };
